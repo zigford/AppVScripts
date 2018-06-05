f7403d0a1b2b8401adb74798a6a0d7a42c317b62
Jesse Harris <jpharris@usc.edu.au>
2018-06-05 13:15:20 +1000

    Added Recipe for RecFind DRM

---
+++ Recipes/Knowledgeone Corporation/RecFind DRM/InstallSoftware.bat
@@ -0,0 +1,13 @@
+SET SOURCE=%~dp0..\Source
+SET WORK=%~dp0
+
+cd "%SOURCE%"
+echo Installing Software
+RF6_29_DRM.exe /S /v"/qb CM_SERVERNAME=\"recfind.usc.edu.au\" CM_SYSTEMNAME=\"RecFind\""
+
+echo Installing Config File
+copy "%WORK%Configuration Files\PROD\RecFind_DRM_629\DRM.exe.config" "C:\Program Files (x86)\Knowledgeone Corporation\RecFind 6\DRM\" /y
+
+echo Deleting bad shortcuts
+del "C:\Users\Public\Desktop\RecFind 6 DRM.lnk"
+ren "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Knowledgeone Corporation\RecFind 6\DRM.lnk" "DRM 6.2.9.lnk"
---
+++ Recipes/Knowledgeone Corporation/RecFind DRM/Readme.md
@@ -0,0 +1,8 @@
+# RecFind 6 DRM 2.9
+Script files to create production package of RecFind DRM.
+
+Author: Jesse Harris
+
+## Discovery
+Uses installsheild setup exe which can be silenced using the standard /S /v/qn params.
+Environment can be customized after installation by replacing DRM.exe.config manifest file in installdir

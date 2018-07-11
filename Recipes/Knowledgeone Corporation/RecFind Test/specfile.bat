212edcccfe74a2050298a0436cdc8b0594866d2c
Jesse Harris <jpharris@usc.edu.au>
2018-07-11 20:20:59 +1000

    K1Library.dll patch, include template to auto exclude appdata

--- Recipes/Knowledgeone Corporation/RecFind Test/specfile.bat
+++ Recipes/Knowledgeone Corporation/RecFind Test/specfile.bat
@@ -1,6 +1,6 @@
 Set INSTALLER=InstallSoftware.bat
-Set PACKAGENAME=KnowledgeOneCorporation_RecFind6 Test_2.9_APPV_Site_USR
+Set PACKAGENAME=KnowledgeOneCorporation_RecFind6 Test_2.9 Rev 2_APPV_Site_USR
 Set BUILDLOGS=C:\BUILDLOGS
 REM Set PREREQS=prereqs.bat
 REM Set PVAD=C:\Program Files\InstallDir
-REM Set APPVTemplate=appvtemplate.appvt
+Set APPVTemplate=appvtemplate.appvt

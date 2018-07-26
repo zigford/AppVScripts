f2c59bb32727c5360e1079349c4cace3f7658879
Jesse Harris <jpharris@usc.edu.au>
2018-07-26 16:44:05 +1000

    Added patch to prod package

--- Recipes/Knowledgeone Corporation/RecFind/InstallSoftware.bat
+++ Recipes/Knowledgeone Corporation/RecFind/InstallSoftware.bat
@@ -8,6 +8,11 @@ RF6_29_Client.exe /S /v/qb
 echo Installing Config File
 copy "%WORK%Configuration Files\PROD\RecFind_Client629\RecFind6.exe.config" "C:\Program Files (x86)\Knowledgeone Corporation\RecFind 6\Client" /y
 
+echo Patching client
+copy "%SOURCE%\Patch\*.*" "C:\Program Files (x86)\Knowledgeone Corporation\RecFind 6\Client" /y
+
 echo Deleting bad shortcuts
 del "C:\Users\Public\Desktop\RecFind 6.lnk"
+
+echo Updating shortcut name for build
 ren "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Knowledgeone Corporation\RecFind 6\RecFind 6.lnk" "RecFind 6 2.9.lnk"

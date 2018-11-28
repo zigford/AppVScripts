92ba3fd5cb37175c43dc67f1bbeaf58b3839ce01
Jesse Harris <jpharris@usc.edu.au>
2018-11-28 17:00:49 +1000

    Removed prereqs from template

--- Recipes/Citrix/Provisioning Services Console/7.17/Working Files/specfile.bat
+++ Recipes/Citrix/Provisioning Services Console/7.17/Working Files/specfile.bat
@@ -2,6 +2,6 @@ Set INSTALLER=InstallSoftware.bat
 Set PACKAGENAME=Citrix_Provisioning Services Console_7.17_APPV_Restricted_WKS
 Set BUILDLOGS=C:\BUILDLOGS
 
-Set PREREQS=prereqs.bat
+REM Set PREREQS=prereqs.bat
 REM Set PVAD=C:\Program Files\InstallDir
 REM Set APPVTemplate=appvtemplate.appvt

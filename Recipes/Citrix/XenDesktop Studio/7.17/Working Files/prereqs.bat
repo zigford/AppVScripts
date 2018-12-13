bfe02620210bab36784fbe0647951d7f4844ebc8
Jesse Harris <jpharris@usc.edu.au>
2018-12-13 11:55:01 +1000

    Fixed typos in prereq

--- Recipes/Citrix/XenDesktop Studio/7.17/Working Files/prereqs.bat
+++ Recipes/Citrix/XenDesktop Studio/7.17/Working Files/prereqs.bat
@@ -3,6 +3,6 @@ Set SOURCE=%~dp0..\Source\Support
 Echo Installing prereqs from "%SOURCE%"
 
 echo VCREDIST2015X64
-start /wait "" "%SOURCE%\VcRedist_2015\vcredist_x64.exe" /q /norestart 
+start /wait "" "%SOURCE%\VcRedist_2015\vc_redist.x64.exe" /q /norestart 
 echo VCREDIST2015X86
-start /wait "" "%SOURCE%\VcRedist_2015\vcredist_x86.exe" /q /norestart 
+start /wait "" "%SOURCE%\VcRedist_2015\vc_redist.x86.exe" /q /norestart 
